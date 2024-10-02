87b15fb7cb0dc43e4903aba165e4816218412bb4
Samson7 <sams.777@yandex.ru>
2024-10-02 07:43:03 +0300

    Update create-arch-bootstrap.sh

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -7,7 +7,7 @@
 
 # Package groups
 audio_pkgs="alsa-lib lib32-alsa-lib alsa-plugins lib32-alsa-plugins libpulse \
-	lib32-libpulse jack2 lib32-jack2 alsa-tools alsa-utils pipewire lib32-pipewire"
+	lib32-libpulse jack2 lib32-jack2 alsa-tools alsa-utils pipewire lib32-pipewire lib32-pulseaudio pipewire-pulse"
 
 video_pkgs="mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon \
 	vulkan-intel lib32-vulkan-intel \
@@ -35,20 +35,16 @@ devel_pkgs="base-devel git meson mingw-w64-gcc cmake"
 # Apart from packages from the official Arch repos, you can also specify
 # packages from the Chaotic-AUR repo
 export packagelist="${audio_pkgs} ${video_pkgs} ${wine_pkgs} ${devel_pkgs} \
-	nano ttf-dejavu ttf-liberation lutris steam firefox mpv geany pcmanfm \
-	htop qbittorrent speedcrunch gpicview file-roller xorg-xwayland \
-	steam-native-runtime gamemode lib32-gamemode jre17-openjdk lxterminal \
-	steamtinkerlaunch mangohud lib32-mangohud qt6-wayland wayland \
-	lib32-wayland qt5-wayland retroarch xorg-server-xephyr openbox \
-	obs-studio gamehub minigalaxy legendary gamescope prismlauncher yt-dlp \
-	bottles playonlinux minizip retroarch-assets-ozone libretro-beetle-psx-hw \
-	libretro-blastem libretro-bsnes libretro-dolphin duckstation \
-	libretro-gambatte libretro-melonds libretro-mgba libretro-nestopia \
-	libretro-parallel-n64 libretro-pcsx2 libretro-picodrive libretro-ppsspp \
-	libretro-retrodream libretro-yabause sunshine nautilus"
+	nano ttf-dejavu ttf-liberation mpv geany pcmanfm \
+	htop gpicview file-roller xorg-xwayland \
+	gamemode lib32-gamemode jre17-openjdk lxterminal \
+	mangohud lib32-mangohud qt6-wayland wayland lutris nasm yasm clisp \
+	lib32-wayland qt5-wayland xorg-server-xephyr openbox lib32-gtk2 lib32-libxi lib32-libxmu lib32-glu glu libxmu \
+	obs-studio yt-dlp sdl12-compat lib32-sdl12-compat sdl_sound sdl_gfx sdl_image grim audacity emacs-wayland kitty \
+	minizip blender freecad fpc nim figlet pcre dictd gnome-dictionary zlib p7zip crawl-tiles cataclysm-dda-tiles stardict zig unarchiver glow steam qt6-multimedia-ffmpeg hyprpicker"
 
 # If you want to install AUR packages, specify them in this variable
-export aur_packagelist=""
+export aur_packagelist="koreader-bin ncurses5-compat-libs lib32-ncurses5-compat-libs meowsans-ttf frogmouth goldendict-ng qalculate-gtk-git kew lutgen-bin gowall paleta tcolors"
 
 # ALHP is a repository containing packages from the official Arch Linux
 # repos recompiled with -O3, LTO and optimizations for modern CPUs for
